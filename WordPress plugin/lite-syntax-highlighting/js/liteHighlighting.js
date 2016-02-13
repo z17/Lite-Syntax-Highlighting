6b7c55240a0b971f9b0e699202f5cf365886fead
z17 <admin@blweb.ru>
2016-02-13 23:29:44 +0300

    update keywords

--- WordPress plugin/lite-syntax-highlighting/js/liteHighlighting.js
+++ WordPress plugin/lite-syntax-highlighting/js/liteHighlighting.js
@@ -9,10 +9,267 @@
  License: GPLv2 or later
  License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
+ 
+var phpKeywords = [
+'unset',
+'print',
+'return',
+'require_once',
+'require',
+'list',
+'isset',
+'include_once',
+'include',
+'eval',
+'exit',
+'empty',
+'echo',
+'die',
+'yield',
+'trait',
+'insteadof',
+'__NAMESPACE__',
+'__METHOD__',
+'__FUNCTION__',
+'__LINE__',
+'__FILE__',
+'__DIR__',
+'__CLASS__',
+'__TRAIT__',
+'__halt_compiler',
+'abstract',
+'and',
+'array',
+'as',
+'break',
+'callable',
+'case',
+'catch',
+'class',
+'clone',
+'const',
+'continue',
+'declare',
+'default',
+'do',
+'else',
+'elseif',
+'enddeclare',
+'endfor',
+'endforeach',
+'endif',
+'endswitch',
+'endwhile',
+'extends',
+'final',
+'finally',
+'for',
+'foreach',
+'function',
+'global',
+'goto',
+'if',
+'implements',
+'instanceof',
+'interface',
+'namespace',
+'new',
+'or',
+'private',
+'protected',
+'public',
+'static',
+'switch',
+'throw',
+'try',
+'use',
+'var',
+'while',
+'xor',
+'implode',
+'sizeof',
+'json_encode',
+'json_decode',
+'array_unique',
+'array_map',
+'array_search',
+'strtotime',
+'array_merge',
+'__construct',
+'explode',
+'date',
+'file_get_contents',
+'preg_split',
+'define'
+];
+var htmlTags = [
+'html',
+'title',
+'body',
+'h1',
+'p',
+'br',
+'hr',
+'acronym',
+'abbr',
+'address',
+'b',
+'bdo',
+'big',
+'blockquote',
+'center',
+'cite',
+'code',
+'del',
+'dfn',
+'em',
+'font',
+'i',
+'ins',
+'kbd',
+'mark',
+'meter',
+'pre',
+'progress',
+'q',
+'rp',
+'rt',
+'ruby',
+'s',
+'samp',
+'small',
+'strike',
+'strong',
+'sub',
+'sup',
+'time',
+'tt',
+'u',
+'var',
+'wbr',
+'form',
+'input',
+'textarea',
+'button',
+'select',
+'optgroup',
+'option',
+'label',
+'fieldset',
+'legend',
+'datalist',
+'keygen',
+'output',
+'frame',
+'frameset',
+'noframes',
+'iframe',
+'img',
+'map',
+'area',
+'canvas',
+'figcaption',
+'figure',
+'audio',
+'source',
+'video',
+'aside',
+'a',
+'link',
+'nav',
+'ul',
+'ol',
+'li',
+'dir',
+'dl',
+'dt',
+'dd',
+'menu',
+'table',
+'caption',
+'th',
+'tr',
+'td',
+'thead',
+'tbody',
+'tfoot',
+'col',
+'colgroup',
+'style',
+'div',
+'span',
+'header',
+'footer',
+'section',
+'article',
+'details',
+'summary',
+'head',
+'meta',
+'base',
+'basefont',
+'script',
+'noscript',
+'applet',
+'embed',
+'object',
+'param'
+];
+var jsKeywords = [
+'auto',
+'double',
+'int',
+'struct',
+'break',
+'else',
+'long',
+'switch',
+'case',
+'enum',
+'register',
+'typedef',
+'char',
+'var',
+'extern',
+'return',
+'union',
+'const',
+'float',
+'short',
+'unsigned',
+'continue',
+'for',
+'signed',
+'void',
+'default',
+'GOTO',
+'sizeof',
+'volatile',
+'do',
+'if',
+'static',
+'while',
+'function',
+'delete',
+'catch',
+'in',
+'this',
+'while',
+'instanceof',
+'throw',
+'with',
+'finally',
+'let',
+'try',
+'debugger',
+'new',
+'typeof',
+];
 function backlightHtml(txt) {
     var comments = [];
     var quotes = [];
     var attr = [];
+	var htmlTagsReg = new RegExp('(&lt;\/?(' + htmlTags.join('|') + ')(&gt;|\\s))', 'g');
     txt = txt
         .replace(/(&lt;!--[\s\S]*?--&gt;)/g, function (str) {
             var l = comments.length;
@@ -34,7 +291,7 @@ function backlightHtml(txt) {
             attr.push(str2);
             return '~~~ATTR' + l + '~~~';
         })
-        .replace(/(&lt;\/?(table|td|tr|span|div|html|header|body|iframe|br|img|a|strong|meta|title|head|pre|code|link|li|ul|ol|p|h1|h2|h3|h4|h5|script|b)(&gt;)?)/g, '<span class="code-tags">$1</span>')
+        .replace(htmlTagsReg, '<span class="code-tags">$1</span>')
         .replace(/~~~(COMM)([0-9]*?)~~~/g, function (reg, str, num) {
             return '<span class="code-comments">' + comments[num] + '</span>';
         })
@@ -47,10 +304,10 @@ function backlightHtml(txt) {
     ;
     return txt;
 }
-
 function backlightPHP(txt) {
     var comments = [];
     var quotes = [];
+	var phpOperatorReg = new RegExp('(\\s|\\!|\\(|^|\\t)(' + phpKeywords.join('|') + ')(\\s|\\(|$|;)', 'g');
     txt = txt
         .replace(/\/\*[\s\S]*?\*\//g, function (str) {
             var l = comments.length;
@@ -74,8 +331,7 @@ function backlightPHP(txt) {
         })
         //   .replace(/(\$[a-zA-Z0-9-_]*?)(\s|,|-|;|\[|\.|{|\(|\)|\])/g, '<span class="code-var">$1</span>$2')
         .replace(/(\$\w*?)(\s|\W)/g, '<span class="code-var">$1</span>$2')
-        .replace(/(\s|\!|\(|^|\t)(function|class|private|implode|new|sizeof|json_encode|json_decode|array|array_unique|array_map|array_search|strtotime|unset|array_merge|public|__construct|array|foreach|class|static|empty|else|elseif|if|break|return|exit|isset|while|explode|date|echo|as|file_get_contents|preg_split)(\s|\(|$|;)/g, '$1<span class="code-operator">$2</span>$3')
-
+        .replace(phpOperatorReg, '$1<span class="code-operator">$2</span>$3')
         .replace(/(&lt;\?php)/g, '<span class="code-php">$1</span>')
         .replace(/(\?&gt;)/g, '<span class="code-php">$1</span>')
         .replace(/~~~(COMM)([0-9]*?)~~~/g, function (reg, str, num) {
@@ -87,16 +343,16 @@ function backlightPHP(txt) {
     ;
     return txt;
 }
-
 function backlightCSS(txt) {
     var comments = [];
+	var htmlTagsReg = new RegExp('(\\s|\\n|^)(' + htmlTags.join('|') + ')(\\s|,|:[a-zA-Z]|\\.|{|#)', 'g');
     txt = txt
         .replace(/\/\*[\s\S]*?\*\//g, function (str) {
             var l = comments.length;
             comments.push(str);
             return '~~~COMM' + l + '~~~';
         })
-        .replace(/( |\n|^)(table|td|tr|span|div|html|header|body|img|a|strong|pre|code|li|ul|ol|p|a|h1|h2|h3|h4|h5|blockquote)( |,|:[a-zA-Z]|\.|{|#)/g, '$1<span class="code-tags">$2</span>$3')
+        .replace(htmlTagsReg, '$1<span class="code-tags">$2</span>$3')
         .replace(/:(link|active|hover|visited|first-child|last-child|before|after)/g, ':<span class="code-pseudo">$1</span>')
         .replace(/\.([a-zA-Z0-9-_]*?)(\s|,|:|\.|{)/g, '.<span class="code-class">$1</span>$2')
         .replace(/\#([a-zA-Z0-9-_]*?)(\s|,|:|\.|{)/g, '#<span class="code-id">$1</span>$2')
@@ -107,22 +363,21 @@ function backlightCSS(txt) {
     ;
     return txt;
 }
-
 function backlightJs(txt) {
-
     var comments = [];
+	var jsKeywordsReg = new RegExp('(\\(|\\s|\\!|^)(' + jsKeywords.join('|') + ')(\\)|\\s|\\!|=|\\()', 'g');
     txt = txt
         .replace(/\/\*[\s\S]*?\*\//g, function (str) {
             var l = comments.length;
             comments.push(str);
             return '~~~COMM' + l + '~~~';
         })
-        .replace(/(^[\\]?\/\/[^\n\r]*(\n|\r\n))/g, function (str) {
+        .replace(/(\/\/[^\n\r]*(\n|\r\n))/g, function (str) {
             var l = comments.length;
             comments.push(str);
             return '~~~COMM' + l + '~~~';
         })
-        .replace(/(\(|\s|\!|^)(auto|double|int|struct|break|else|long|switch|case|enum|register|typedef|char|extern|return|union|const|float|short|unsigned|continue|for|signed|void|default|GOTO|sizeof|volatile|do|if|static|while)(\)|\s|\!|=|\()/g, '$1<span class="code-operator">$2</span>$3')
+        .replace(jsKeywordsReg, '$1<span class="code-operator">$2</span>$3')
         .replace(/(=|\s|\(|\t)"([^(code\-)].*?)"/g, '$1<span class="code-quotes">"$2"</span>')
         .replace(/\'([^(code\-)].*?)\'/g, '<span class="code-quotes">\'$1\'</span>')
         .replace(/~~~(COMM)([0-9]*?)~~~/g, function (reg, str, num) {
@@ -131,9 +386,7 @@ function backlightJs(txt) {
     ;
     return txt;
 }
-
 function backlightC(txt) {
-
     var comments = [];
     txt = txt
         .replace(/\/\*[\s\S]*?\*\//g, function (str) {
@@ -157,38 +410,32 @@ function backlightC(txt) {
     ;
     return txt;
 }
-
 jQuery(document).ready(function () {
-    jQuery('pre.html, pre.css, pre.php, pre.js').each(function () {
-
+    jQuery('pre.html, pre.css, pre.php, pre.js, pre.slh__html, pre.slh__css, pre.slh__js, pre.slh__php, pre.slh__c').each(function () {
         var safe = {
             '<': '&lt;',
             '>': '&gt;'
         };
         var txt = jQuery(this).html();
         txt = txt
-            .replace("<!--?php", '<?php')
-            .replace("?-->", '?>')
+            .replace(/<!--\?php/g, '<?php')
+            .replace(/\?-->/g, '?>')
             .replace(/[\<\>]/g, function (m) {
                 return safe[m];
             });
         jQuery(this).html(txt);
     });
-
     jQuery('pre.html, pre.slh__html').each(function () {
         var txt = jQuery(this).html();
         txt = backlightHtml(txt);
         jQuery(this).html(txt);
     });
-
     jQuery('pre.css, pre.slh__css').each(function () {
         var txt = jQuery(this).html();
         txt = backlightCSS(txt);
         jQuery(this).html(txt);
     });
-
     jQuery('pre.php, pre.slh__php').each(function () {
-
         var php = [];
         var txt = jQuery(this).html();
         txt = txt
@@ -209,17 +456,14 @@ jQuery(document).ready(function () {
         }
         jQuery(this).html(txt);
     });
-
     jQuery('pre.js, pre.slh__js').each(function () {
         var txt = jQuery(this).html();
         txt = backlightJs(txt);
         jQuery(this).html(txt);
     });
-
     jQuery('pre.slh__c').each(function () {
         var txt = jQuery(this).html();
         txt = backlightC(txt);
         jQuery(this).html(txt);
     });
-
 });
